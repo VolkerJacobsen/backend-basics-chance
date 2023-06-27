52f361ca086d6fcb7e074af82ee765ce23865bd1
Volker Jacobsen <volkerjacobsen@gmail.com>
2023-06-27 13:33:00 +0200

    solution

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((request, response) => {
+  const name = chance.name();
+  const profession = chance.profession();
+  const age = chance.age();
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+  );
+});
